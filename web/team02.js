897dcf9d39c3d2da6df84044d241fb59baf001f4
David Donovan <daviddonovan5@gmail.com>
2019-01-16 20:29:24 -0800

    team activity

---
+++ web/team02.js
@@ -0,0 +1,11 @@
+function clicked(){
+
+	alert("clicked");
+}
+
+function changeColor(){
+
+	var x = document.getElementById("color").value;
+	document.getElementById("div1").style.background=x;
+}
+
