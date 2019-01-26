0d0d5e532964cd58c99b10a2f4cdb0ec075ac90a
David Donovan <daviddonovan5@gmail.com>
2019-01-25 20:51:55 -0800

    adding files for checkout

--- web/wk3/shopping.php
+++ web/wk3/shopping.php
@@ -42,40 +42,19 @@ session_start();
         <td><img class="itemPic" src="widget2.png" alt="Lens Kit" width="120px" height="120px"></td>
         <td><b>Mobile Phone Camera Lens Kit</b>--This is a detachable jelly lens set for mobile phones including a Fish Eye Lens, Macro Lens,  and a Wide Angle lens. </td> 
         <td>$48.00<br>
-        Quantity: <br><input id="Quantity2" type="number" name="widget2" min="0" max="20" value="<?php
-        if(isset($_POST['addCart'])){
-          echo($_SESSION['quantity2']);
-        }
-        else{
-          echo('0');
-        }
-        ?>"</td>
+        Quantity: <br><input id="Quantity2" type="number" name="widget2" min="0" max="20" value="<?php echo($_SESSION['quantity2']);?>"</td>
       </tr>
       <tr>
         <td><img class="itemPic" src="widget3.png" alt="magical moon lamp" width="120px" height="120px"></td>
         <td><b>Creative 3D Magical Moon Lamp</b>--This is a light that will illuminate to appear like the moon. The battery is re-chargle through a usb cord (included). </td> 
         <td>$25.00<br>
-        Quantity: <br><input id="Quantity3" name="widget3" type="number" min="0" max="20" value="<?php
-        if(isset($_POST['addCart'])){
-          echo($_SESSION['quantity3']);
-        }
-        else{
-          echo('0');
-        }
-        ?>" </td>
+        Quantity: <br><input id="Quantity3" name="widget3" type="number" min="0" max="20" value="<?php echo($_SESSION['quantity3']);?>" </td>
       </tr>
       <tr>
         <td><img class="itemPic" src="widget4.png" alt=head lamp" width="120px" height="120px"></td>
         <td><b>4 Modes Rainproof Mechanical Zoom Headlamp</b>--This is the ultimate headlamp with 5 LED lights can be as bright as 90,000Lm. This headlamp is perfect for all outdoors activities, and will free up your hands. </td> 
         <td>$95.00<br>
-        Quantity: <br><input id="Quantity4" name="widget4" type="number" min="0" max="20" value="<?php
-        if(isset($_POST['addCart'])){
-          echo($_SESSION['quantity4'] );
-        }
-        else{
-          echo('0');
-        }
-        ?>" </td>
+        Quantity: <br><input id="Quantity4" name="widget4" type="number" min="0" max="20" value="<?php echo($_SESSION['quantity4'] );?>" </td>
       </tr></tr></table>
  
 <input type="submit" value="Add to Cart" name="addCart" class="navagation" />
