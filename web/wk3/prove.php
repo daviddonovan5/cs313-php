fe18aed0b4bc06f450cbd11b26537a1d9114d24a
David Donovan <daviddonovan5@gmail.com>
2019-01-25 20:28:37 -0800

    adding files for checkout

--- web/wk3/prove.php
+++ web/wk3/prove.php
@@ -47,8 +47,8 @@ $_SESSION['quantity4'] = $quant4;
         <td><b>Multifunctional OTG Card Reader</b>--Supported memory card: SD, TF, MicroSD.</td> 
         <td>$12.00<br>
         Quantity:<br> <input id="Quantity1" type="number" name="widget1" min="0" max="20" value="<?php
-        //if(isset($_POST['addCart'])){
-          echo($_SESSION['quantity1']. "</td><td>$" . $_SESSION['quantity1'] * 12.00);
+        if(isset($_POST['addCart'])){
+          echo($_SESSION['quantity1']);
         }
         else{
           echo('0');
