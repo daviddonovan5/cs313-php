b35f8978429e6d9a4820bd26e2d20dee56ba68c7
David Donovan <daviddonovan5@gmail.com>
2019-02-08 21:07:17 -0800

    working on ward history

--- web/history/index.php
+++ web/history/index.php
@@ -27,15 +27,33 @@ session_start();
     <h4>Enter Information For a New Event</h4><hr/>
 
       <h5> Select the Orginization/Auxiliary</h5>
-      <input type="radio" name="org" value="bisopric"> <label class="container">Bishopric<br>
-      <input type="radio" name="org" value="elders_quorum"> <label class="container">Elders Quorum<br>
-      <input type="radio" name="org" value="primary"> <label class="container">Primary<br>
-      <input type="radio" name="org" value="relief_society"> <label class="container">Relief Society<br>
-       <input type="radio" name="org" value="Sunday School"> <label class="container">Sunday School<br>
-      <input type="radio" name="org" value="activities"> <label class="container">Ward Activities<br>
-      <input type="radio" name="org" value="missionary"> <label class="container">Ward Missionary<br>
-      <input type="radio" name="org" value="young_men"> <label class="container">Young Men<br>
-      <input type="radio" name="org" value="young_women"> <label class="container">Young Women<br>
+
+      <label class="container">One
+        <input type="radio" checked="checked" name="radio">
+        <span class="checkmark"></span>
+      </label>
+      <label class="container">Two
+        <input type="radio" name="radio">
+        <span class="checkmark"></span>
+        </label>
+      <label class="container">Three
+        <input type="radio" name="radio">
+        <span class="checkmark"></span>
+      </label>
+      <label class="container">Four
+        <input type="radio" name="radio">
+        <span class="checkmark"></span>
+      </label>
+      
+      <input type="radio" name="org" value="bisopric"> Bishopric<br>
+      <input type="radio" name="org" value="elders_quorum"> Elders Quorum<br>
+      <input type="radio" name="org" value="primary"> Primary<br>
+      <input type="radio" name="org" value="relief_society"> Relief Society<br>
+       <input type="radio" name="org" value="Sunday School"> Sunday School<br>
+      <input type="radio" name="org" value="activities"> Ward Activities<br>
+      <input type="radio" name="org" value="missionary"> Ward Missionary<br>
+      <input type="radio" name="org" value="young_men"> Young Men<br>
+      <input type="radio" name="org" value="young_women"> Young Women<br>
       
      
      
