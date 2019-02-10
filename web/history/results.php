7b4428052985462199d4cd5afe580bfb8add2552
David Donovan <daviddonovan5@gmail.com>
2019-02-09 17:06:33 -0800

    working on ward history

--- web/history/results.php
+++ web/history/results.php
@@ -47,7 +47,7 @@
   foreach ($db->query('SELECT act_date, title, description FROM event') as $row)
   {
     //print_r($row);
-    echo "<p><b> <CENTER>" . $row['act_date'] . "{$row['title']} </b></CENTER> <br>";
+    echo "<p><b> <CENTER>" . $row['act_date'] . " {$row['title']} </b></CENTER> <br>";
     echo $row['description'];
     echo '"</p>';
   }
