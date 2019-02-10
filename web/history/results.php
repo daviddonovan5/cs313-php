20d5cefdccebf4e85e844ed24b0d9baf42111e43
David Donovan <daviddonovan5@gmail.com>
2019-02-09 20:19:44 -0800

    working on ward history

--- web/history/results.php
+++ web/history/results.php
@@ -53,19 +53,31 @@
         FROM event
         INNER JOIN users ON event.user_id = users.user_id
         INNER JOIN organization ON users.organization_id = organization.organization_id') as $row)
-  {
-    //print_r($row);
-    echo "<p><b> <CENTER>" . $row['act_date'] . " {$row['title']} <br>{$row['organization_name']} </b></CENTER> </p>";
-    echo "<p>" .$row['description'] . "</p>";
-    
-  }
-          
+        {
+        //print_r($row);
+          echo "<p><b> <CENTER>" . $row['act_date'] . " {$row['title']} <br>{$row['organization_name']} </b></CENTER> </p>";
+          echo "<p>" .$row['description'] . "</p>";
+        }
+      }
+      else{
+
+        include "db_connect.php";
+        
+        foreach ($db->query('SELECT event.act_date, event.title, event.description, organization.organization_name, users.first_name, users.last_name
+        FROM event
+        INNER JOIN users ON event.user_id = users.user_id
+        INNER JOIN organization ON users.organization_id = organization.organization_id'
+        WHERE organization.orgization_id = $_POST["org"]) as $row)
+        {
+        //print_r($row);
+          echo "<h3>Results for </h3>" . $row['organization_name'];
+        
+          echo "<p><b> <CENTER>" . $row['act_date'] . " {$row['title']} <br>{$row['organization_name']} </b></CENTER> </p>";
+          echo "<p>" .$row['description'] . "</p>";
         }
 
-
-
-
-      } 
+      }
+    } 
 
 
     
