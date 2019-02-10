87bae80a0be8db57467ec4b0ffc855b239a0294f
David Donovan <daviddonovan5@gmail.com>
2019-02-09 20:27:37 -0800

    working on ward history

--- web/history/results.php
+++ web/history/results.php
@@ -45,9 +45,9 @@
   <?php
       
       if (isset($_POST["org"])){
-       
+       include "db_connect.php";
         if ($_POST["org"] == 0){
-        include "db_connect.php";
+        
         echo "<h3> All the Results</h3>";
         foreach ($db->query('SELECT event.act_date, event.title, event.description, organization.organization_name, users.first_name, users.last_name
         FROM event
@@ -62,20 +62,7 @@
       else{
         echo("wow");
 
-       // include "db_connect.php";
-        
-       // foreach ($db->query('SELECT event.act_date, event.title, event.description, organization.organization_name, users.first_name, users.last_name
-        //FROM event
-        //INNER JOIN users ON event.user_id = users.user_id
-        //INNER JOIN organization ON users.organization_id = organization.organization_id'
-        //WHERE orginzation_id='{$_POST["org"]}') as $row)
-       // {
-        //print_r($row);
-        //  echo "<h3>Results for </h3>" . $row['organization_name'];
-        
-          //echo "<p><b> <CENTER>" . $row['act_date'] . " {$row['title']} <br>{$row['organization_name']} </b></CENTER> </p>";
-          //echo "<p>" .$row['description'] . "</p>";
-        //}
+       
 
       }
     } 
