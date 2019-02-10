7c2c64b3b50b89dfafe788a5623ff913754cf98a
David Donovan <daviddonovan5@gmail.com>
2019-02-09 20:36:42 -0800

    working on ward history

--- web/history/results.php
+++ web/history/results.php
@@ -64,7 +64,7 @@
             FROM event
             INNER JOIN users ON event.user_id = users.user_id
             INNER JOIN organization ON users.organization_id = organization.organization_id
-            WHERE organization_id = 1") as $row)
+            WHERE organization_id = 3") as $row)
         {
         //print_r($row);
           
