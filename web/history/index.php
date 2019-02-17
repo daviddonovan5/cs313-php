c30b0906d24ddb9cfe44bdea15c090c707dca192
David Donovan <daviddonovan5@gmail.com>
2019-02-16 22:06:35 -0800

    working on database mod

--- web/history/index.php
+++ web/history/index.php
@@ -122,7 +122,7 @@ catch (Exception $ex)
   // Please be aware that you don't want to output the Exception message in
   // a production environment
  // echo "Error with DB. Details: $ex";
-//  die();
+ die();
 }
 
 }
