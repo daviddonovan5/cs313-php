5a5d45255a5f556cf7a0c22a91363e435fb5faa6
David Donovan <daviddonovan5@gmail.com>
2019-02-16 20:18:11 -0800

    working on database mod

--- web/history/index.php
+++ web/history/index.php
@@ -24,7 +24,23 @@
     
     <h4>Enter Information For a New Event</h4><hr/>
 
-      
+      <span class="prompt"> Enter your first name </span><br>
+      <input type="text" name="fname"><br>
+      <span class="prompt"> Enter your last name </span><br>
+      <input type="text" name="lname"><br>
+      <span class="prompt"> Select the orginization </span><br>
+      <select name="org">
+      <option value=0>All</option>
+      <option value=1>Bishopric</option>
+      <option value=3>Elders Quorum</option>
+      <option value=6>Primary</option>
+      <option value=2>Relief Society</option>
+      <option value=8>Sunday School</option>
+      <option value=9>Ward Activities</option>
+      <option value=7>Ward Missionary</option>
+      <option value=4>Young Men</option>
+      <option value=5>Young Women</option>
+      </select>
       <span class="prompt"> Enter the Date of the activity </span><br>
       <input type="date" name="date"><br>
       <span class="prompt"> Enter the Name of the activity </span><br>
