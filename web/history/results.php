a45a16d8f6adbfc0105122853159c890d80c9e00
David Donovan <daviddonovan5@gmail.com>
2019-02-23 21:02:47 -0800

    working on database mod

--- web/history/results.php
+++ web/history/results.php
@@ -89,7 +89,7 @@
   if(isset($_POST['delete'])){
     
     
-      //$db->query("$deleteQuery= "DELETE FROM event WHERE event_id = $_POST[hidden]");
+      $db->query("DELETE FROM event WHERE event_id = $_POST[hidden]");
       echo "EVENT DELETED";
     };
 
